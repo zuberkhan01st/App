6999dd0f91d9352b17636651c17c4afe4b17a838
Zuber Khan <zuberkhan01st@gmail.com>
2024-11-28 03:37:02 +0530

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Toast from 'react-native-toast-message';  // Import the Toast library
+import HomeScreen from './src/screens/HomeScreen';
+import LoginScreen from './src/screens/LoginScreen';
+import AdminScreen from './src/screens/AdminScreen';
+import PostmasterScreen from './src/screens/PostmasterScreen';
+import SweeperScreen from './src/screens/SweeperScreen';
+import ManagePostScreen from './src/screens/ManagePostScreen';
+import ViewInboxScreen from './src/screens/ViewInboxScreen';
+
+const Stack = createStackNavigator();
 
 export default function App() {
+  const [expoPushToken, setExpoPushToken] = useState('');
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="LoginScreen" component={LoginScreen} />
+        <Stack.Screen name="AdminScreen" component={AdminScreen} />
+        <Stack.Screen name="PostmasterScreen" component={PostmasterScreen} />
+        <Stack.Screen name="SweeperScreen" component={SweeperScreen} />
+        <Stack.Screen name="ManagePostScreen" component={ManagePostScreen}/>
+        <Stack.Screen name="ViewInboxScreen" component={ViewInboxScreen}/>
+      </Stack.Navigator>
+
+      {/* Updated Toast Component */}
+      <Toast />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
